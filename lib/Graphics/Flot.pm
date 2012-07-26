ffb74798028b38b68cf70b160b47af43b00aceb4
Joel Berger <joel.a.berger@gmail.com>
2012-07-26 17:18:46 -0500

    Lots of changes. This isn't a real project yet anyway

--- lib/Graphics/Flot.pm
+++ lib/Graphics/Flot.pm
@@ -3,23 +3,65 @@ package Graphics::Flot;
 use strict;
 use warnings;
 
-use JSON 'encode_json';
 use Scalar::Util 'looks_like_number';
 use Carp;
 
-our $Validate = 1;
+use base 'Exporter';
+our @EXPORT = qw/plot/;
 
 sub plot {
-  my $datasets = @_ == 1 ? shift () : [ @_ ];
+  my @datasets = @_;
 
-  if ($Validate) {
-    for my $dataset (@$datasets) {
-      my $data = eval { ref $dataset eq 'HASH' } ? $dataset->{data} : $dataset;
-      _validate_data($data);
+  my @output_datasets;
+  for my $dataset (@datasets) {
+    # promote to hashref
+    unless (eval { ref $dataset eq 'HASH' }) {
+      $dataset = { data => $dataset };
     }
+
+    # 1D piddle
+    if (eval { $dataset->{data}->isa('PDL') }) {
+      $dataset->{data} = [ $dataset->{data}->sequence, $dataset->{data} ];
+    }
+
+    # promote two piddles
+    if ( eval { $dataset->{data}->[0]->isa('PDL') } and eval { $dataset->{data}->[1]->isa('PDL') } ) {
+      my ($x, $y) = @{ $dataset->{data} };
+      my $data = _unroll($x->cat($y)->xchg(0,1));
+      $dataset->{data} = $data;
+    }
+
+    # promote a 1D list
+    if ( not ref $dataset->{data}->[0]) {
+      my $x = 0;
+      my @pairs;
+      for my $y (@{ $dataset->{data} }) {
+        if ( ! defined $y ) {
+          $x--;
+          push @pairs, undef;
+          next;
+        }
+
+        push @pairs, [$x++, $y];
+      }
+      $dataset->{data} = \@pairs;
+    }
+
+    _validate_data($dataset->{data});
+
+    push @output_datasets, $dataset;
   }
 
-  return encode_json( $datasets );
+  return +{ plot => \@output_datasets };
+}
+
+sub _unroll {
+  my $pdl = shift;
+  if ($pdl->ndims > 1) {
+    return [ map { _unroll($_) } $pdl->dog ];
+  } else {
+    return [$pdl->list];
+  }
 }
 
 sub _validate_data {
@@ -41,4 +83,10 @@ sub _validate_point {
 }
 
 1;
+
+__END__
+
+allow [2,4,6,undef, 10] => [[0,2],[1,4],[3,6], undef, [3,10]]
+allow $pdl_y
+allow [$pdl_x, $pdl_y]
 
